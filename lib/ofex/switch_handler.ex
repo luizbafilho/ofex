279cb2e66a333b74875e52765fcc8aa9c86ecf30
Luiz Filho <luizbafilho@gmail.com>
2016-01-09 00:02:20 -0200

    wip

--- lib/ofex/switch_handler.ex
+++ lib/ofex/switch_handler.ex
@@ -2,6 +2,7 @@ require Logger
 
 defmodule Ofex.SwitchHandler do
   use GenServer
+  alias OfProto.Messages.Hello
 
   @port 6633
 
@@ -10,8 +11,11 @@ defmodule Ofex.SwitchHandler do
   end
 
   def handle_cast({:new_switch, socket}, {}) do
-    spawn_link(fn -> recv_loop(socket) end)
     Logger.info("New switch!")
+
+    :gen_tcp.send(socket, Hello.encode(%Hello{}))
+
+    spawn_link(fn -> recv_loop(socket) end)
     {:noreply, {}}
   end
 
@@ -32,10 +36,7 @@ defmodule Ofex.SwitchHandler do
 
   def accept_loop(socket) do
     {:ok, client} = :gen_tcp.accept(socket)
-    # {:ok, task} = Task.start_link(fn -> handle_connection(client) end)
-    # :ok = :gen_tcp.controlling_process(client, task)
     GenServer.cast(__MODULE__, {:new_switch, client})
-    # :ok = :gen_tcp.controlling_process(socket, pid)
     accept_loop(socket)
   end
 
@@ -45,41 +46,7 @@ defmodule Ofex.SwitchHandler do
   end
 
   def recv_message(socket) do
-    {:ok, <<version::size(8), type::size(8), length::size(16), xid::size(32)>>} = :gen_tcp.recv(socket, 8)
-    body = case length - 8 do
-      0 ->
-        <<>>
-      l ->
-        {:ok, p} = :gen_tcp.recv(socket, l)
-    end
-
-    Logger.info version
-    Logger.info type
-    Logger.info length
-    :gen_tcp.send(socket, <<version::size(8), type::size(8), length::size(16), xid::size(32)>>)
-    # case :gen_tcp.recv(socket, 0, :infinity) do
-    #   {:ok, data} ->
-    #     IO.puts data
-    #   what ->
-    #     IO.inspect what
-    # end
-    # handle_connection(socket)
+    {:ok, data } = :gen_tcp.recv(socket, 0)
+    IO.inspect data
   end
-
-  # def loop(socket, transport) do
-  #   case transport.recv(socket, 0, :infinity) do
-  #     {:ok, data} ->
-  #       IO.puts "Message from switch"
-  #       # hello = %OfProto.Messages.Hello{}
-  #       # transport.send(socket, hello |> OfProto.Messages.Hello.encode)
-  #       hello = OfProto.Messages.Hello.decode(data)
-  #       transport.send(socket, hello |> OfProto.Messages.Hello.encode)
-  #       features = %OfProto.Messages.FeaturesRequest{xid: 394239292}
-  #       transport.send(socket, features |> OfProto.Messages.FeaturesRequest.encode)
-  #       # IO.inspect features
-  #       # transport.send(socket, msg |> OfProto.Messages.Hello.encode)
-  #     what ->
-  #       IO.inspect what
-  #   end
-  # end
 end
